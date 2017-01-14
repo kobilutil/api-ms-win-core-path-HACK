c3f64f51d67e665d876af96e25ddb783a3222ffb
kobilutil <kobilutil@users.noreply.github.com>
2017-01-14 08:53:33 +0200

    implement PathCchCanonicalizeEx, PathCchRemoveFileSpec
    - implement PathCchCanonicalizeEx as call to PathCanonicalize
    - implement PathCchRemoveFileSpec as a call to PathRemoveFileSpec

--- api-ms-win-core-path-HACK.cpp
+++ api-ms-win-core-path-HACK.cpp
@@ -2,5 +2,20 @@
 //
 
 #include "stdafx.h"
+#include <Shlwapi.h>
+#pragma comment(lib, "Shlwapi.lib")
+
+
+HRESULT PathCchCanonicalizeEx(PWSTR pszPathOut, size_t cchPathOut, PCWSTR pszPathIn, unsigned long dwFlags)
+{
+	auto ret = PathCanonicalize(pszPathOut, pszPathIn);
+	return ret ? S_OK : E_FAIL;
+}
+
+HRESULT PathCchRemoveFileSpec(PWSTR pszPath, size_t cchPath)
+{
+	auto ret = PathRemoveFileSpec(pszPath);
+	return ret ? S_OK : E_FAIL;
+}
 
 
